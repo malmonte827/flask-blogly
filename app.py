7bbf4592c1dad6e315d284588fcfe6eb2dfedde2
Michael <malmonte454@proton.me>
2024-07-11 19:02:18 -0400

    Intial commit

---
+++ app.py
@@ -0,0 +1,18 @@
+"""Blogly application."""
+
+from flask import Flask, render_template, redirect, request
+from models import db, connect_db, User
+from flask_debugtoolbar import DebugToolbarExtension
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+app.config['SECRET_KEY'] = 'verysecret'
+
+debug = DebugToolbarExtension(app)
+connect_db(app)
+
+
---
+++ models.py
@@ -0,0 +1,34 @@
+"""Models for Blogly."""
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+default_profile_img = "https://www.freeiconspng.com/uploads/name-people-person-user-icon--icon-search-engine-1.png"
+
+def connect_db(app):
+    """ Connect to database """
+
+    db.app = app
+    db.init_app(app)
+
+
+class User(db.Model):
+    """ User """
+
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer,
+                   primary_key=True,
+                   autoincrement=True)
+    
+    first_name = db.Column(db.String(25),
+                           nullable=False)
+    
+    last_name = db.Column(db.String(25),
+                          nullable=False)
+    
+    image_url = db.Column(db.String,
+                          nullable=False,
+                          default=default_profile_img)
+    
+    
