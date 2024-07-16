ef5976fe371eab8ded575b284336d1c25ab1a17f
Michael <malmonte454@proton.me>
2024-07-15 23:30:46 -0400

    Added new class Posts

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 """Models for Blogly."""
 from flask_sqlalchemy import SQLAlchemy
+import datetime
 
 db = SQLAlchemy()
 
@@ -30,6 +31,27 @@ class User(db.Model):
                           nullable=False,
                           default=default_profile_img)
     
+    posts = db.relationship('Post', backref='user')
+    
     def full_name(self):
         """ Returns full name of users """
         return f'{self.first_name} {self.last_name}'
+    
+class Post(db.Model):
+    """ Post """
+
+    __tablename__ = 'posts'
+
+    id = db.Column(db.Integer,
+                   primary_key=True,
+                   autoincrement=True)
+    title = db.Column(db.String(50),
+                      nullable=False)
+    content = db.Column(db.String,
+                        nullable=False)
+    created_at = db.Column(db.DateTime,
+                           nullable=False,
+                           default=datetime.datetime.now)
+    user_id = db.Column(db.Integer,
+                        db.ForeignKey('users.id'),
+                        nullable=False)
